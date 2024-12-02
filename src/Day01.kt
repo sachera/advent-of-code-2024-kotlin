efd527cc30e7f978694856666efe1bbfcd2d46aa
sachera <github@a-sacher.de>
2024-12-02 23:26:34 +0100

    Day 01 solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,21 +1,22 @@
+import kotlin.math.absoluteValue
+
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
-
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
-
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
     // Or read a large test input from the `src/Day01_test.txt` file:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val (listA, listB) = readInput("Day01").map {
+        val (fst,snd) = it.split("\\s+".toRegex())
+        Pair(fst.toInt(),snd.toInt())
+    }.unzip()
 
-    // Read the input from the `src/Day01.txt` file.
-    val input = readInput("Day01")
-    part1(input).println()
-    part2(input).println()
+    val diffSum = listA.sorted().zip(listB.sorted()).sumOf { (a, b) ->
+        (a - b).absoluteValue
+    }
+    println(diffSum)
+
+    val testSet = listA.toSet()
+    val similarity = listB.filter { testSet.contains(it) }
+        .groupBy { it }
+        .map { it.key * it.value.size }
+        .sum()
+
+    println(similarity)
 }
