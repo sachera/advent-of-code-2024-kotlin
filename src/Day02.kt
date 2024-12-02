ccbc96a0900372716b85e612ab94e4287c09aea2
sachera <github@a-sacher.de>
2024-12-02 23:47:13 +0100

    Day 02 solution

---
+++ src/Day02.kt
@@ -0,0 +1,30 @@
+import kotlin.math.absoluteValue
+import kotlin.math.sign
+
+fun main() {
+    val input = readInput("Day02")
+
+    fun isSafe(report: List<Int>): Boolean {
+        val zipped = report.zipWithNext()
+        val expectedSign = (zipped[0].first - zipped[0].second).sign
+        val isMonotone = zipped.all { (a,b) -> (a-b).sign == expectedSign }
+        val isGraduallyChanging = zipped.all { (a,b) -> a != b && (a-b).absoluteValue <= 3 }
+        return isMonotone && isGraduallyChanging
+    }
+
+    val safeReports = input.map { line ->
+        line.split(" ").map { it.toInt() }
+    }.count { report ->
+        isSafe(report)
+    }
+    println(safeReports)
+
+    val safeReportsWithDampener = input.map { line ->
+        line.split(" ").map { it.toInt() }
+    }.count { report ->
+        isSafe(report) || report.indices.any { i ->
+            isSafe(report.take(i) + report.drop(i+1))
+        }
+    }
+    println(safeReportsWithDampener)
+}
